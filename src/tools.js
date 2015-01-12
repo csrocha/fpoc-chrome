908a11d3f9d8de6ea402dbf005035caebc252fa8
Cristian S. Rocha <cristian.rocha@moldeo.coop>
2015-01-12 20:15:15 -0300

    Add printer Hasar smh/p-441f to list of supported printers with protocol Epson-D. Fixing #5.

--- src/tools.js
+++ src/tools.js
@@ -69,9 +69,10 @@ var local_printers = {};
 var local_devices = {};
 
 var supported_printers = [
-    { port: 'usb', protocol: 'epson_e_ar', vendorId: 1208, productId: 514 },
-    { port: 'serial', protocol: 'epson_e_ar' },
-    { port: 'serial', protocol: 'epson_d_ar' },
+    { port: 'usb', protocol: 'epson_e_ar', vendorId: 1208, productId: 514 },   // TM-U220 AF II (v.22.01) Demeter
+    { port: 'usb', protocol: 'epson_d_ar', vendorId: 1027, productId: 24577 }, // smh/p-441f
+    { port: 'serial', protocol: 'epson_e_ar' }, // TM-U220 AF II (v.22.01) Demeter
+    { port: 'serial', protocol: 'epson_d_ar' }, // TM U220 AF II 
     ];
 
 var query_local_printers = function(callback, onchange) {
